dd118850dab7b6de9aa13ce736652bdcb85ac9d0
franklin fargoj <franklin.fargoj@neosofttech.com>
2019-07-09 10:25:07 +0530

    09july

--- src/app/edit-profile/edit-profile.component.ts
+++ src/app/edit-profile/edit-profile.component.ts
@@ -13,16 +13,20 @@ export class EditProfileComponent implements OnInit {
   username='';
   email='';
   existingpassword='';
-  newPassword='';
+//  newPassword='';
   user_id='';
+  submitted = false;
+
 
   updateprofileForm:FormGroup = new FormGroup({
-    email:new FormControl(null,[Validators.email,Validators.required]),
-    username:new FormControl(null,Validators.required),
-    existingpassword: new FormControl(null,Validators.required),
-    newPassword:new FormControl(null,Validators.required) ,
-    user_id:new FormControl(null,Validators.required) ,
+      email:new FormControl(null,[Validators.email,Validators.required]),
+      username:new FormControl(null,Validators.required),
+      existingpassword: new FormControl(null,Validators.required),
+    // newPassword:new FormControl(null,Validators.required) ,
+      user_id:new FormControl(null,Validators.required) 
   })
+
+  get f() { return this.updateprofileForm.controls; }
 
   constructor(private _router:Router,private _user:UserService) { 
     this._user.user()
@@ -31,15 +35,13 @@ export class EditProfileComponent implements OnInit {
     )
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   displayProfile(data){ 
     this.username = data.username;
     this.email = data.email;
     //this.password=data.password;
     this.user_id= data._id;
-
   }
 
   redirect_ontodos(){
@@ -47,19 +49,20 @@ export class EditProfileComponent implements OnInit {
   }
 
   updated_profile_data(){
+    this.submitted = true;
     //console.warn(this.updateprofileForm.value);
     this._user.profileUpdate(JSON.stringify(this.updateprofileForm.value))
     .subscribe(
-      data=> {
-         
-       
-         console.log(data);
-         this._router.navigate(['/login']);
-         console.log('I am in updated_profile_data frontend');
-        
-
-         error=> console.error(error);
-         })
+        data=> {
+          console.log(data);
+          if(data == true){
+            console.log('Profile updated');
+            this._router.navigate(['/user']);
+          }else{
+            console.log('Profile not updated');
+          }
+          }
+    )
   }
   
 
