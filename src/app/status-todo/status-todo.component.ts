920939fcd8dc85d9204e0c286849c259c9b4b081
franklin fargoj <franklin.fargoj@neosofttech.com>
2019-07-24 13:34:20 +0530

    Emitter added in status todo

--- src/app/status-todo/status-todo.component.ts
+++ src/app/status-todo/status-todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input  } from '@angular/core';
+import { Component, OnInit, Input, Output ,EventEmitter,OnChanges } from '@angular/core';
 import { UserService } from '../user.service';
 
 @Component({
@@ -9,23 +9,28 @@ import { UserService } from '../user.service';
 export class StatusTodoComponent implements OnInit {
 
   @Input() childMessage: string;
-
   status:String='';
+  task_done:any='';
+  @Input() private uploadSuccess: EventEmitter<any>;
+
 
   constructor(private _user:UserService) { }
 
   ngOnInit() {
     this.getSubtitles(this.childMessage);
+    this.uploadSuccess.subscribe(()=>{
+        this.getSubtitles(this.childMessage);
+    }) 
   }
 
   getSubtitles(childMessage){ 
-    this._user.listSubtitles(this.childMessage).subscribe( data=> {   
-      if(data.headertodo.length ==  data.listofsubtitles.length  &&data.listofsubtitles.length > 0){
-        this.status = 'Completed';
-       }
+      this._user.listSubtitles(JSON.stringify({head_id:this.childMessage,token: localStorage.getItem('access_token')})).subscribe( data=> {
+      if(data.listofsubtitles.length > 0){
+        this.task_done = ((data.headertodo.length/data.listofsubtitles.length)*100).toFixed(2);
+
+      }
       }
     )
   }
-
 
 }
