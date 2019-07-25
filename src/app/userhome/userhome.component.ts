6ee0eedf036432d75198ce63ffa56a5c9bc6c313
franklin fargoj <franklin.fargoj@neosofttech.com>
2019-07-25 14:52:12 +0530

    replaced user()

--- src/app/userhome/userhome.component.ts
+++ src/app/userhome/userhome.component.ts
@@ -3,7 +3,6 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-userhome',
   templateUrl: './userhome.component.html',
@@ -12,34 +11,22 @@ import { Router } from '@angular/router';
 export class UserhomeComponent implements OnInit {
   
   username:String='';
-  @Input('work_completed') work_completed: String;
   work:String='';
+  @Input('work_completed') work_completed: String;
 
   constructor(private _user:UserService, private _router:Router) {
     // this.work = _user.work_completed;
-    this._user.user().subscribe(
+    this._user.logged_in_user(localStorage.getItem('access_token')).subscribe(
       data=>this.addName(data),
       error=>this._router.navigate(['/login'])
     )
   }
 
-  ngOnInit() {
-
-  }
+  ngOnInit() { }
 
   addName(data){ 
     //console.log(data);   
     this.username = data.username;
-  }
-
-  logout(){
-    this._user.logout().subscribe(
-      data=>{
-        localStorage.clear();
-        this._router.navigate(['/login'])
-      },
-      error=>console.error(error)
-    )
   }
 
   update_redirect(){
